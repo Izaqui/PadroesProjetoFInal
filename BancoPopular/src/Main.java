4ec367c95b9621253943e50e92f8cf6da5fe93f8
Izaqui <izaquiel.silva.ads@gmail.com>
2021-09-22 08:23:04 -0300

    Correção do codigo

--- BancoPopular/src/Main.java
+++ BancoPopular/src/Main.java
@@ -1,4 +1,17 @@
 import java.util.Scanner;
+class Conta{
+    private float saldo;
+    private float limite;
+    public void saca(int valor) {
+		
+	}
+	
+    public void deposita(int valor) {
+
+    }
+}
+    
+
 public class Main {
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
