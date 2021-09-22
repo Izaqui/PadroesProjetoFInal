68234340586b61f06729be5c44292b554d872f7c
Izaqui <izaquiel.silva.ads@gmail.com>
2021-09-22 10:06:50 -0300

    Inicio da implementação dos metodos

--- BancoPopular/src/Main.java
+++ BancoPopular/src/Main.java
@@ -3,14 +3,28 @@ class Conta{
     private float saldo;
     private float limite;
     public void saca(int valor) {
-		
+		if(saldo<valor){
+            System.out.print("Seu saldo é insuficiente para este saque");
+        }
+        else{
+            System.out.println("Saque de "+valor+"R$ efetuado com sucesso");
+        }
 	}
 	
     public void deposita(int valor) {
 
     }
+    public void transferir(){
+
+    }
 }
-    
+class FactoryConta {
+
+	public Conta getConta(float limite, float valor) {
+		
+	}
+
+}    
 
 public class Main {
     public static void main(String[] args){
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Padrões de pojeto "Banco Popula"
 com o intuito de utilizar padrões de projetos em uma aplicação simples 
+o padrão utilizado na etapa cooperativa sera o "Factory Method"
